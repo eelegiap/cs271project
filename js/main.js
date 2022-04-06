fb40c05214fc3d4b4d8f26b0ceab302fc4e63701
Fiona Henry <fhenry@g.harvard.edu>
2022-04-06 09:46:48 -0400

    first pass at having sidebar change

--- js/main.js
+++ js/main.js
@@ -34,9 +34,6 @@ function initMainPage(allDataArray) {
 
 
     myText = new TextPanel(sent_order, word_align);
-    myAlignmentBar = new AlignmentBar('alignmentBar', source_align, translation_align, sent_order);
-    myTimeline = new Timeline('timeline', source_align, translation_align, sent_order);
-    myNGrams = new nGrams("En", "In");
 }
 
 
@@ -50,4 +47,56 @@ function updateSidebar(cur_source_align, cur_translation_align){
     myAlignmentBar.updateVis(source_lemma, translation_lemma);
     myTimeline.updateVis(source_lemma, translation_lemma);
     myNGrams = new nGrams(cur_source_align, cur_translation_align);
+}
+
+function createWordLevelSidebar(){
+    myAlignmentBar = new AlignmentBar('alignmentBar', source_align, translation_align, sent_order);
+    myTimeline = new Timeline('timeline', source_align, translation_align, sent_order);
+    myNGrams = new nGrams("En", "In");
+}
+
+function createSentenceLevelSidebar(){
+    console.log("Placeholder for sentencce level sidebar")
+}
+
+function switchSidebar(bar_type){
+    let analysis_panel = document.getElementById("aPanel")
+    while (analysis_panel.firstChild) {
+        analysis_panel.removeChild(analysis_panel.firstChild);
+    }
+    if(bar_type == "wordlevel") {
+        analysis_panel.innerHTML += "<div class=\"row\" style=\"height: 45vh\">\n" +
+            "\t\t\t\t\t\t<div id=\"nextButton\"></div>\n" +
+            "\t\t\t\t\t\t<div id=\"alignmentBar\" style=\"height: 5vh\">\n" +
+            "\t\t\t\t\t\t</div>\n" +
+            "\t\t\t\t\t\t<div id=\"alignmentTitle\">\n" +
+            "\t\t\t\t\t\t</div>\n" +
+            "\t\t\t\t\t\t<div id=\"textArea\" style=\"height: 35vh\">\n" +
+            "\t\t\t\t\t\t</div>\n" +
+            "\t\t\t\t\t</div>\n" +
+            "\t\t\t\t\t<div class=\"row\" style=\"height: 30vh\">\n" +
+            "\t\t\t\t\t\t<table id='ngramtitle' style=\"width:100%; color: gray\"></table>\n" +
+            "\t\t\t\t\t\t<div id='ngramviewer'>\n" +
+            "\t\t\t\t\t\t</div>\n" +
+            "\t\t\t\t\t</div>\n" +
+            "\t\t\t\t\t<div class=\"row\" style=\"height: 10vh\">\n" +
+            "\t\t\t\t\t\t<div id=\"timeline\">\n" +
+            "\t\t\t\t\t\t</div>\n" +
+            "\t\t\t\t\t</div>\n" +
+            "\t\t\t\t</div>";
+        createWordLevelSidebar();
+    }
+    else{
+        analysis_panel.innerHTML += "\t\t\t\t\t<div class=\"row\" style=\"height: 30vh\">\n" +
+            "\t\t\t\t\t\t<p>Word Frequency</p>\n" +
+            "\t\t\t\t\t</div>\n" +
+            "\t\t\t\t\t<div class=\"row\" style=\"height: 30vh\">\n" +
+            "\t\t\t\t\t\t<p>Sentence Length</p>\n" +
+            "\t\t\t\t\t</div>\n" +
+            "\t\t\t\t\t<div class=\"row\" style=\"height: 30vh\">\n" +
+            "\t\t\t\t\t\t<p>Lexical Richness</p>\n" +
+            "\t\t\t\t\t</div>"
+        createSentenceLevelSidebar();
+    }
+
 }
